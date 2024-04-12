b6cbdf6942c4d7dc9a09a826e3093327870adf45
Sharukhi <sharukhi@sharukhi.xyz>
2024-04-12 23:03:41 +0600

    Add adapter-vercel

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,10 +1,12 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: [vitePreprocess({})],
 
   kit: {
+    adapter: adapter({}),
     alias: {
       $components: "src/components",
       $lib: "src/components",
