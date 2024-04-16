9b491bc69dcaaa6cd1f9ed81a095588fe2ef4b50
Sharukhi <sharukhi@sharukhi.xyz>
2024-04-16 12:55:58 +0600

    Add TWA

---
+++ android/app/src/main/java/app/khobor/sharukhi/xyz/DelegationService.java
@@ -0,0 +1,14 @@
+package app.khobor.sharukhi.xyz;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
